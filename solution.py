57b1bff006f0f86083d0c439ace02b84671da857
Rus <iYablokoG@gmail.com>
2023-04-12 19:31:40 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -2,13 +2,16 @@ import pandas as pd
 import numpy as np
 
 
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 38897891 # Ваш chat ID, не меняйте название переменной
 
 def solution(x_success: int, 
              x_cnt: int, 
              y_success: int, 
              y_cnt: int) -> bool:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return ... # Ваш ответ, True или False
+    from scipy.stats import norm
+    
+    f = x_success / x_cnt
+    s = y_success / y_cnt
+    btw = np.sqrt((f * (1 - f) / x_cnt) + (s * (1-s) / y_cnt))
+    res = (s - f) / btw > norm.ppf(1-0.09)
+    return res
